0261236af62d1e12c06339168a65bac48ac2b0a4
cwiklam <cwikla.m0@gmail.com>
2024-10-27 14:37:47 +0100

    Added method to create_user and superuser + tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,18 +1,31 @@
-from django.db import models # noqa
+from django.db import models  # noqa
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """ Manager for users """
+
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ User in the system """
